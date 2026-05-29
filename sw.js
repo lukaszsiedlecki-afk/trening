ecad5680a34f3170bb017665bd12b62c50a581a7
Lukasz <lukasz.siedlecki@ccig.pl>
2026-05-29 10:09:52 +0200

    Ok91

--- sw.js
+++ sw.js
@@ -5,16 +5,34 @@ const ASSETS = [
   './manifest.json'
 ];
 
-// Instalacja i zapisywanie plików do pamięci podręcznej
+// Instalacja i natychmiastowe wymuszenie nowej wersji
 self.addEventListener('install', (e) => {
   e.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
       return cache.addAll(ASSETS);
+    }).then(() => {
+      return self.skipWaiting(); // MODYFIKACJA v9: Nie czekaj na zamknięcie kart deaktywujących aplikację
     })
   );
 });
 
-// Serwowanie plików z pamięci, gdy nie ma internetu
+// Aktywacja i agresywne usuwanie poprzednich cache-y
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    }).then(() => {
+      return self.clients.claim(); // MODYFIKACJA v9: Natychmiastowe przejęcie kontroli nad aplikacją
+    })
+  );
+});
+
 self.addEventListener('fetch', (e) => {
   e.respondWith(
     caches.match(e.request).then((response) => {
